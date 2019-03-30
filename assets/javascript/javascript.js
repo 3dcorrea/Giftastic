0817d5aaf894f2687c563d0cf82ff327e255021c
Dan Correa <dancorrea3@gmail.com>
2019-03-30 18:02:06 -0400

    Js and index alterations/comments added

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,45 +1,53 @@
-var topics = ["Cheeses", "Wines", "Cars", "Spongebob", "Memes", "Haduken", "Kamehameha", ];
+// For some reason, the gifs arent showing up on the dom. I cant figure out what I'm missing/doing incorrectly.
+
+var topics = ["Cheeses", "Wines", "Cars", "Spongebob", "Memes", "Haduken", "Kamehameha", "Random", ];
 
 function showTopicInfo() {
     var topic = $(this).attr("data-name");
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=r1gNhXP5d6BYyLMWx6Z6yYMnpXMvdvF8&q=" + topic + "&limit=10&offset=0&rating=G&lang=en";
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=r1gNhXP5d6BYyLMWx6Z6yYMnpXMvdvF8&q=" + topic + "&limit=10&offset=0&rating=G&lang=en";
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        console.log(response);
         var topicDiv = $("<div class='topic'>");
-        var rating = response.Rated;
+        var rating = response.data.Rated;
         var paragraph = $("<p>").text("Rating: " + rating);
         topicDiv.append(paragraph);
-        var imgURL = result.images;
+        var imgURL = response.data.Images;
         var image = $("<img>").attr("src", imgURL);
         topicDiv.append(image);
-        $("#topics-view").append(topicDiv);
+        $("#topics-view").empty();
+        $("#topics-view").append(topicDiv, rating, paragraph, imgURL, image);
     });
 };
 
 function createButtons() {
-    $("#topics-view").empty();
+    $("#button-view").empty();
     for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
         a.addClass("topic");
         a.attr("data-name", topics[i]);
         a.text(topics[i]);
-
-        $("#topics-view").append(a);
+        $("#button-view").append(a);
     };
 };
+createButtons();
 
+// I keep getting an error message for line 40 but I cant figure out why
 $("#add-topic").on("click", function (event) {
     event.preventDefault();
     var topic = $("#topic-input").val().trim();
     topics.push(topic);
-    createButtons();
+    createButtons(topic);
 });
-createButtons();
 
 
-//IM STILL WORKING ON OTHER THINGS BEFORE I TURN THIS ON
+
+//I took this from the solved example in class but I'm unable to implement it because gifs arent loading in the dom
+
 // $(".gif").on("click", function() {
 //     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
 //     var state = $(this).attr("data-state");
